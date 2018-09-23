5a44daa0c2186dca400db86054d08022fd115e8f
Conrado Chiesa <conradochiesa@gmail.com>
2018-09-22 21:01:52 -0300

    Primera parte de refactorización.

---
+++ config/ConfigApp.php
@@ -0,0 +1,16 @@
+<?php
+
+class ConfigApp
+{
+    public static $ACTION = 'action';
+    public static $PARAMS = 'params';
+    public static $ACTIONS = [
+      ''=> 'personajesController#Home',
+      'home'=> 'personajesController#Home',
+      'borrar'=> 'personajesController#Borrarpersonaje',
+      'completada'=> 'personajesController#Completarpersonaje'
+    ];
+
+}
+
+ ?>
